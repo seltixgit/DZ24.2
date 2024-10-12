83b9342aeec83ff82751ddae854a847b8e6690a4
Denis <seltix9381@gmail.com>
2024-10-12 17:11:32 +0300

    commit dz25.1 update

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
 
 from users.apps import UsersConfig
-from users.views import PaymentViewSet, UserCreateAPIView
+from users.views import PaymentViewSet, UserCreateAPIView, UserListAPIView, UserRetrieveAPIView, UserUpdateAPIView, \
+    UserDestroyAPIView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -18,6 +19,10 @@ urlpatterns = [
     path('register/', UserCreateAPIView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
+    path("user/", UserListAPIView.as_view(), name="user_list"),
+    path("user/<int:pk>/", UserRetrieveAPIView.as_view(), name="user_retrieve"),
+    path("user/<int:pk>/update/", UserUpdateAPIView.as_view(), name="user_update"),
+    path("user/<int:pk>/delete/", UserDestroyAPIView.as_view(), name="user_delete"),
 ]
 
 urlpatterns += router.urls
